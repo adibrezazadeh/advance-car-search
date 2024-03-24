e47ca5bdb861a0411d146f0305080d6fe55f5460
Adib rezazadeh <adib.rezazadeh@gmail.com>
2024-03-24 11:46:19 +0330

    fix Features & Options

--- pages/[vehicleBase].jsx
+++ pages/[vehicleBase].jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { httpRequest } from "@/apis/index";
 import SliderSwip from "@/Components/Slider/SliderSwip";
-import styles from '@/Components/Slider/SliderSwip.module.css';
+import styles from "@/Components/Slider/SliderSwip.module.css";
 import Details from "@/Components/Details/Details";
+import { NavItem } from "react-bootstrap";
 
 function vehicleBase({ data, data2, domain: host, specialData }) {
   console.log("dataaaa====", data);
@@ -16,7 +17,9 @@ function vehicleBase({ data, data2, domain: host, specialData }) {
   return (
     <div className="container">
       {/* header of page */}
-      <div className={`d-flex justify-content-between align-items-center mt-4 ${styles.header}`}>
+      <div
+        className={`d-flex justify-content-between align-items-center mt-4 ${styles.header}`}
+      >
         <div className="d-flex gap-1 align-items-center">
           <p className={styles.headertop}>{data.Vehicle.model_year}</p>
           <p className={styles.headertop}>{data.Vehicle.make}</p>
@@ -28,14 +31,34 @@ function vehicleBase({ data, data2, domain: host, specialData }) {
       </div>
       {/* data of car picture and details */}
       <div className="d-flex row">
+        {/* pics of cars */}
         <div className="col-12 col-lg-6">
-          <SliderSwip data2={data2}/>
-
+          <SliderSwip data2={data2} />
+          {/* icons behind of picture */}
+          <div></div>
         </div>
+        {/* details of car */}
         <div className="col-12 col-lg-6">
-          <Details data={data}/>
+          <Details data={data} />
         </div>
-
+      </div>
+      {/* discription */}
+      <div className="mt-3">
+        <div className={`fw-bold ${styles.headertop}`}>Description</div>
+        <div>
+          <div dangerouslySetInnerHTML={{ __html: data.comment }} />
+        </div>
+      </div>
+      {/* Features & Options */}
+      <div className="mt-3">
+        <div className={`fw-bold mb-2 ${styles.headertop}`}>Features & Options</div>
+        <div>
+          <div  className={`row col-12`}>
+            {data.more_option.map((item, index) => (
+              <p key={index + 1} className={`col-6 col-md-4 ${styles.options}`}>{item}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
