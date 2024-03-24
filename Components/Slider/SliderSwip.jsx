1cd9e3d631efe03b678085468ca68354945b4793
Adib rezazadeh <adib.rezazadeh@gmail.com>
2024-03-24 10:42:32 +0330

    fix the swipper select

--- Components/Slider/SliderSwip.jsx
+++ Components/Slider/SliderSwip.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from 'swiper/modules';
 import "swiper/css";
@@ -7,20 +7,34 @@ import styles from './SliderSwip.module.css';
 
 function SliderSwip({data2}) {
  const [activeSlide, setActiveSlide] = useState(0);
+ const swiperMainRef = useRef(null);
+ const swiperTwoRef = useRef(null);
+
+ const handleSlideClick = (index) => {
+   setActiveSlide(index);
+   if (swiperMainRef.current) {
+     swiperMainRef.current.slideTo(index);
+   }
+ };
+
  return (
     <div className={`${styles.sliderbox} mt-5 col-12`}>
       <Swiper
+        ref={swiperMainRef}
         className='swipermain'
         navigation={true} 
         modules={[Navigation]}
         spaceBetween={0}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        onSwiper={(swiper) => {
+          // Ensure the Swiper instance is correctly set
+          swiperMainRef.current = swiper;
+        }}
         initialSlide={activeSlide}
       >
         {data2.map((item, index) => (
-          <SwiperSlide key={index} >
+          <SwiperSlide key={index}>
             <img
               src={`https://image123.azureedge.net${item.media_src}`}
               alt={`Slide ${index + 1}`}
@@ -30,20 +44,22 @@ function SliderSwip({data2}) {
         ))}
         </Swiper>
       <Swiper
+        ref={swiperTwoRef}
         className='mt-3 swipertwo'
         navigation={true} 
         modules={[Navigation]}
         spaceBetween={25}
         slidesPerView={4}
         onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        onSwiper={(swiper) => {
+          swiperTwoRef.current = swiper;
+        }}
         initialSlide={activeSlide}
         >
         {data2.map((item, index) => (
-          <SwiperSlide key={index} >
+          <SwiperSlide key={index} onClick={() => handleSlideClick(index)}>
             <img
-            className={`${index===activeSlide?styles.activeImage:""} `}
-              onClick={() => setActiveSlide(index)}
+              className={`${index===activeSlide?styles.activeImage:""} `}
               src={`https://image123.azureedge.net${item.media_src}`}
               alt={`Slide ${index + 1}`}
               style={{ width: '100%', height: 'auto' }}
